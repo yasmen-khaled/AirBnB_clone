122c9fb562e98af92adbf0a588c042cd31d68164
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-12-05 21:51:44 +0300

    Done!

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,58 @@
 #!/usr/bin/python3
+
+import json
+import os
+from models.base_model import BaseModel
+
+class FileStorage():
+   
+
+    __file_path = "file.json"
+    __objects = {}
+
+    """______ all - method  selected all objects _______"""
+
+    def all(self):
+
+        return FileStorage.__objects
+    
+    """______ new - method  creating a new  object to __object dict _______"""
+
+    def new(self, obj):
+
+        new_entry = obj.__class__.__name__ + '.' + obj.id
+
+        FileStorage.__objects[new_entry] = obj
+ 
+    """______ save - Serializeing converting from py to json _______"""
+
+    def save(self):
+    
+        creat_dict = {
+    
+            obj: FileStorage.__objects[obj].to_dict()
+            for obj in FileStorage.__objects.keys()
+
+            }
+        
+        with open(FileStorage.__file_path, 'w') as x:
+            json.dump(creat_dict, x)
+
+        """______reloading - Deserializeing converting from json to py _______"""
+
+    def reload(self):
+
+        if FileStorage.__file_path is None:
+            return
+        try:
+            with open(FileStorage.__file_path) as x:
+                if os.path.getsize(FileStorage.__file_path) == 0:
+                    return
+                creat_dict = json.load(x)
+                for key in creat_dict.values():
+                    class_name = key["__class__"]
+                    del key["__class__"]
+                    
+                    self.new(eval(class_name)(**key))
+        except FileNotFoundError:
+            return
