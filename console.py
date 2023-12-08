b120d5c9534f47b4535c5d442e3a03cab7c8dc14
lujain <lujainm.com>
2023-12-08 03:44:00 -0800

    AirBnB

--- console.py
+++ console.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 """Modules"""
 import cmd
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
     """class for consolo in airbnb"""
 
     prompt = "(hbnb) "
+    __cl = {
+            "Review",
+            "Place",
+            "State",
+            "User",
+            "BaseModel",
+            "Amenity",
+            "City"
+            }
 
     def do_EOF(self, lin):
         """EOF command to exit the program"""
@@ -23,6 +40,18 @@ class HBNBCommand(cmd.Cmd):
         """do nothing"""
 
         pass
+
+    def do_create(self, lin):
+        """to create"""
+
+        firs = lin.split()
+        if len(firs) == 0:
+            print("** class name missing **")
+        elif firs[0] not in HBNBCommand.__cl:
+            print("** class doesn't exist **")
+        else:
+            print(eval(firs[0])().id)
+            models.storage.save()
 
 
 if __name__ == '__main__':
