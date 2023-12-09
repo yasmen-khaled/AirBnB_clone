0540a2119671cc2b356d171957818f09e0c764ef
lujain <lujainm.com>
2023-12-09 10:23:41 -0800

    AirBnB

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 "Modules"
 import unittest
-
+import models
+from models.place import Place
 
 
 class TestPlace(unittest.TestCase):
     """Class Plase Test"""
-    pass
+    
+    def test_noArg(self):
+        self.assertEqual(Place, type(Place()))
+
+    def test_newInstance(self):
+        self.assertIn(Place(), models.storage.all().values())
+
+    def test_id(self):
+        self.assertEqual(str, type(Place().id))
+
 
 if __name__ == "__main__":
     unittest.main()
