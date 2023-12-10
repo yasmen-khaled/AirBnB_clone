822b354ca283f90cc13a90b689247031e3f23dec
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-12-10 15:06:26 +0300

    done

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,89 +2,89 @@
 "Modules"
 import models
 import unittest
-from models.place import Place
+from models.amenity import Amenity
 
 
-class TestPlace(unittest.TestCase):
+class TestAmenity(unittest.TestCase):
     """Class Plase Test"""
 
     def test_no(self):
-        self.assertEqual(Place, type(Place()))
+        self.assertEqual(Amenity, type(Amenity()))
 
     def test_new(self):
-        self.assertIn(Place(), models.storage.all().values())
+        self.assertIn(Amenity(), models.storage.all().values())
 
     def test_id(self):
-        self.assertEqual(str, type(Place().id))
+        self.assertEqual(str, type(Amenity().id))
 
-    def test_city(self):
-        li = Place()
-        self.assertEqual(str, type(Place.city_id))
-        self.assertIn("city_id", dir(li))
-        self.assertNotIn("city_id", li.__dict__)
+    def test_Amenity(self):
+        li = Amenity()
+        self.assertEqual(str, type(Amenity.Amenity_id))
+        self.assertIn("Amenity_id", dir(li))
+        self.assertNotIn("Amenity_id", li.__dict__)
 
-    def test_user(self):
-        li = Place()
-        self.assertEqual(str, type(Place.user_id))
-        self.assertIn("user_id", dir(li))
-        self.assertNotIn("user_id", li.__dict__)
+    def test_Amenity(self):
+        li = Amenity()
+        self.assertEqual(str, type(Amenity.Amenity_id))
+        self.assertIn("Amenity_id", dir(li))
+        self.assertNotIn("Amenity_id", li.__dict__)
 
     def test_name(self):
-        li = Place()
-        self.assertEqual(str, type(Place.name))
+        li = Amenity()
+        self.assertEqual(str, type(Amenity.name))
         self.assertIn("name", dir(li))
         self.assertNotIn("name", li.__dict__)
 
     def test_n(self):
-        li = Place()
-        self.assertEqual(int, type(Place.number_rooms))
+        li = Amenity()
+        self.assertEqual(int, type(Amenity.number_rooms))
         self.assertIn("number_rooms", dir(li))
         self.assertNotIn("number_rooms", li.__dict__)
 
     def test_number(self):
-        pl = Place()
-        self.assertEqual(int, type(Place.number_bathrooms))
+        pl = Amenity()
+        self.assertEqual(int, type(Amenity.number_bathrooms))
         self.assertIn("number_bathrooms", dir(pl))
         self.assertNotIn("number_bathrooms", pl.__dict__)
 
     def test_longitude(self):
-        li = Place()
-        self.assertEqual(float, type(Place.longitude))
+        li = Amenity()
+        self.assertEqual(float, type(Amenity.longitude))
         self.assertIn("longitude", dir(li))
         self.assertNotIn("longitude", li.__dict__)
 
     def test_attribute(self):
-        li = Place()
-        self.assertEqual(list, type(Place.amenity_ids))
-        self.assertIn("amenity_ids", dir(li))
-        self.assertNotIn("amenity_ids", li.__dict__)
+        li = Amenity()
+        self.assertEqual(list, type(Amenity.Amenity_ids))
+        self.assertIn("Amenity_ids", dir(li))
+        self.assertNotIn("Amenity_ids", li.__dict__)
 
     def test_unique(self):
-        k = Place()
-        li = Place()
+        k = Amenity()
+        li = Amenity()
         self.assertNotEqual(k.id, li.id)
         
     def test_max(self):
-        pl = Place()
-        self.assertEqual(int, type(Place.max_guest))
+        pl = Amenity()
+        self.assertEqual(int, type(Amenity.max_guest))
         self.assertIn("max_guest", dir(pl))
         self.assertNotIn("max_guest", pl.__dict__)
 
     def test_price(self):
-        li = Place()
-        self.assertEqual(int, type(Place.price_by_night))
+        li = Amenity()
+        self.assertEqual(int, type(Amenity.price_by_night))
         self.assertIn("price_by_night", dir(li))
         self.assertNotIn("price_by_night", li.__dict__)
 
     def test_latitude(self):
-        li = Place()
-        self.assertEqual(float, type(Place.latitude))
+        li = Amenity()
+        self.assertEqual(float, type(Amenity.latitude))
         self.assertIn("latitude", dir(li))
         self.assertNotIn("latitude", li.__dict__)
 
     def test_instantiation(self):
         with self.assertRaises(TypeError):
-            Place(id=None, created_at=None, updated_at=None)
+            Amenity(id=None, created_at=None, updated_at=None)
 
 
 if __name__ == "__main__":
