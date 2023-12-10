390ccd83007e1ea2c37e1b29af650d076eedbfaf
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-12-10 18:53:11 +0300

    Update console.py

--- console.py
+++ console.py
@@ -153,8 +153,8 @@ class HBNBCommand(cmd.Cmd):
             _object = "{}.{}".format(_list[0], _list[1])
             if _object in storage.all():
                 insta = storage.all()[_object]
-                attr_name = _list[2]
-                val = _list[3][1:-1]
+                attr_name = _list[2][1:-1]
+                val = _list[3]
                 setattr(insta, attr_name, val)
                 storage.save()
             else:
