761ea87f95520b7495347bcb0830560bb43641ab
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-12-10 15:19:42 +0300

    done

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,91 +1,74 @@
-#!/usr/bin/python3
-"Modules"
-import models
 import unittest
+import models
+import os
+from datetime import datetime
+from models.base_model import BaseModel
 from models.amenity import Amenity
 
 
-class TestAmenity(unittest.TestCase):
-    """Class Plase Test"""
+class TestAmenity_instantiation(unittest.TestCase):
+    def setup_amen(self):
+        self.amenity = Amenity()
+        self.amenity.name = "Bob"
+        self.amenity.number = "1"
+        amenity_dict = self.amenity.to_dict()
 
-    def test_no(self):
+
+    def tearDown(self):
+        del self.amenity
+
+    def test_args(self):
         self.assertEqual(Amenity, type(Amenity()))
 
-    def test_new(self):
+    def test_new_instance(self):
+
         self.assertIn(Amenity(), models.storage.all().values())
+    def test_init(self):
+        self.assertIsInstance(self.amenity, BaseModel)
+        self.assertIsInstance(self.amenity.name, str)
+        self.assertEqual(self.amenity.name, "Bob")
 
-    def test_id(self):
-        self.assertEqual(str, type(Amenity().id))
+    def test_str(self):
+        amenity_str = str(self.amenity)
+        self.assertIn("[Amenity]", amenity_str)
+        self.assertIn(self.amenity.id, amenity_str)
+        self.assertIn(str(self.amenity.__dict__), amenity_str)
 
-    def test_Amenity(self):
-        li = Amenity()
-        self.assertEqual(str, type(Amenity.Amenity_id))
-        self.assertIn("Amenity_id", dir(li))
-        self.assertNotIn("Amenity_id", li.__dict__)
+class TestAmenity_to_dict(unittest.TestCase):
 
-    def test_Amenity(self):
-        li = Amenity()
-        self.assertEqual(str, type(Amenity.Amenity_id))
-        self.assertIn("Amenity_id", dir(li))
-        self.assertNotIn("Amenity_id", li.__dict__)
+    def setup(self):
+        self.amenity = Amenity()
+        self.amenity.name = "Bob"
+        self.amenity.number = "1"
+        amenity_dict = self.amenity.to_dict()
 
-    def test_name(self):
-        li = Amenity()
-        self.assertEqual(str, type(Amenity.name))
-        self.assertIn("name", dir(li))
-        self.assertNotIn("name", li.__dict__)
+    def test_type(self):
+        self.assertTrue(dict, type(Amenity().to_dict()))
 
-    def test_n(self):
-        li = Amenity()
-        self.assertEqual(int, type(Amenity.number_rooms))
-        self.assertIn("number_rooms", dir(li))
-        self.assertNotIn("number_rooms", li.__dict__)
+    def test_keys(self):
+        self.assertIn("id", self.amenity.to_dict())
+        self.assertIn("created_at", self.amenity.to_dict())
+        self.assertIn("updated_at", self.amenity.to_dict())
+        self.assertIn("__class__", self.amenity.to_dict())
 
-    def test_number(self):
-        pl = Amenity()
-        self.assertEqual(int, type(Amenity.number_bathrooms))
-        self.assertIn("number_bathrooms", dir(pl))
-        self.assertNotIn("number_bathrooms", pl.__dict__)
+    def test_added_attributes(self):
+        amenity_dict = self.amenity.to_dict()
+        self.assertEqual(amenity_dict["name"], self.amenity.name)
+        self.assertEqual(amenity_dict["number"], self.amenity.number)
 
-    def test_longitude(self):
-        li = Amenity()
-        self.assertEqual(float, type(Amenity.longitude))
-        self.assertIn("longitude", dir(li))
-        self.assertNotIn("longitude", li.__dict__)
+    def test_attributes_type(self):
+        amenity_dict = self.amenity.to_dict()
+        self.assertEqual(type(amenity_dict["id"]), str)
+        self.assertEqual(type(amenity_dict["created_at"]), str)
+        self.assertEqual(type(amenity_dict["updated_at"]), str)
 
-    def test_attribute(self):
-        li = Amenity()
-        self.assertEqual(list, type(Amenity.Amenity_ids))
-        self.assertIn("Amenity_ids", dir(li))
-        self.assertNotIn("Amenity_ids", li.__dict__)
-
-    def test_unique(self):
-        k = Amenity()
-        li = Amenity()
-        self.assertNotEqual(k.id, li.id)
-        
-    def test_max(self):
-        pl = Amenity()
-        self.assertEqual(int, type(Amenity.max_guest))
-        self.assertIn("max_guest", dir(pl))
-        self.assertNotIn("max_guest", pl.__dict__)
-
-    def test_price(self):
-        li = Amenity()
-        self.assertEqual(int, type(Amenity.price_by_night))
-        self.assertIn("price_by_night", dir(li))
-        self.assertNotIn("price_by_night", li.__dict__)
-
-    def test_latitude(self):
-        li = Amenity()
-        self.assertEqual(float, type(Amenity.latitude))
-        self.assertIn("latitude", dir(li))
-        self.assertNotIn("latitude", li.__dict__)
-
-    def test_instantiation(self):
-        with self.assertRaises(TypeError):
-            Amenity(id=None, created_at=None, updated_at=None)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_output(self):
+        dt = datetime.today()
+        amenity_dict = self.amenity.to_dict()
+        self.assertEqual(amenity_dict["id"], self.amenity.id)
+        self.amenity.id = "1-2-3-4"
+        self.assertEqual(amenity_dict["created_at"],
+                         self.amenity.created_at.isoformat())
+        self.assertEqual(amenity_dict["updated_at"],
+                         self.amenity.updated_at.isoformat())
+        self.assertEqual(amenity_dict["name"], self.amenity.name)
